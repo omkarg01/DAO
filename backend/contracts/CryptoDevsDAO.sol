cac2344697d9796d9ecf763439244ccdf4118c82
omkarg01 <omkargujja01@gmail.com>
2023-02-14 13:56:35 +0530

    withdraw ether done

--- backend/contracts/CryptoDevsDAO.sol
+++ backend/contracts/CryptoDevsDAO.sol
@@ -185,7 +185,9 @@ contract CryptoDevsDAO is Ownable {
         payable(owner()).transfer(amount);
     }
 
-    
+    // The following two functions allow the contract to accept ETH deposits
+    // directly from a wallet without calling a function
+    receive() external payable {}
 
-
+    fallback() external payable {}
 }
