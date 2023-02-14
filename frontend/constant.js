a1fb0335f0ceeed315883a0cc2af3e4a6ed7824b
omkarg01 <omkargujja01@gmail.com>
2023-02-14 20:14:09 +0530

    done

--- frontend/constant.js
+++
@@ -1,5 +0,0 @@
-export const CRYPTODEVS_DAO_CONTRACT_ADDRESS = "";
-export const CRYPTODEVS_NFT_CONTRACT_ADDRESS = "";
-
-export const CRYPTODEVS_DAO_ABI = [];
-export const CRYPTODEVS_NFT_ABI = [];
