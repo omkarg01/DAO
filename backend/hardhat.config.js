85914b4c3484a9d5f2c097f2abc0cd6ff039d73c
omkarg01 <omkargujja01@gmail.com>
2023-02-14 14:34:15 +0530

    deploy done

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,3 +1,10 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({ path: ".env" });
+
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
     solidity: "0.8.0",
     defaultNetwork: "goerli",
